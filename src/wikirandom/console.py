1d49d9b5b501cc10f9eb28cb01ddb3604cd8a6c1
Narendra Nag <narendranag@gmail.com>
2023-11-06 11:32:28 +0530

    Put in error handling

--- src/wikirandom/console.py
+++ src/wikirandom/console.py
@@ -14,14 +14,17 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 def main():
     """The wikirandom CLI."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
-    
-    title = data["title"]
-    extract = data["extract"]
+    try:
+        with requests.get(API_URL) as response:
+            if(response.raise_for_status()):
+                raise click.Exception(response.text)
+            else:
+                data = response.json()
+                
+                title = data["title"]
+                extract = data["extract"]
 
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
-    
-
+                click.secho(title, fg="green")
+                click.echo(textwrap.fill(extract))
+    except requests.exceptions.RequestException as e:
+        raise click.ClickException(str(e))
