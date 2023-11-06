83e61df7e22b86477ad2581611a31ab27c6026af
Narendra Nag <narendranag@gmail.com>
2023-11-06 11:21:31 +0530

    Created a console script

---
+++ src/wikirandom/console.py
@@ -0,0 +1,12 @@
+# src/wikirandom/console.py
+
+import click
+from . import __version__
+
+@click.command()
+@click.version_option(version=__version__)
+
+def main():
+    """The wikirandom CLI."""
+    click.echo("Hello, world!")
+    
